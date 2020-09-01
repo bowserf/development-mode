df9d9cde758f5e5252d2418308909dcbbd8f7737
Frédéric Torcheux <frederic.torcheux@djit.fr>
2020-09-01 21:38:36 +0200

    [SDK] Add icon launcher to start DevelopmentModeActivity
    This icon launcher only appears in debug build type.

---
+++ develoment_mode/src/debug/java/fr/bowser/dev/develoment_mode/DevelopmentModeActivity.kt
@@ -0,0 +1,12 @@
+package fr.bowser.dev.develoment_mode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DevelopmentModeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.development_mode_activity)
+    }
+}
